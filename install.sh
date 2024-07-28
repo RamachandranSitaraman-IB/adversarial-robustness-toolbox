153e5d5fbc7842762e3115c92db35912c46b4051
RamachandranSitaraman-IB <sitaraman@styrk.ai>
2024-07-28 10:27:57 -0700

    Support for Pytorch NeuralCleanse

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Installing ART..."
+cd ./dist
+pip uninstall -y adversarial_robustness_toolbox-1.16.0-py3-none-any.whl
+cd ..
+python setup.py bdist_wheel
+cd ./dist
+pip install adversarial_robustness_toolbox-1.16.0-py3-none-any.whl
+echo "ART installed successfully"
+
+
